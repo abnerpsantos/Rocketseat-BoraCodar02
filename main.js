fcd4bd0b3a337190722306ba08e77698991a2ab9
Abner Santos <88912494+abnerpsantos@users.noreply.github.com>
2023-01-16 11:54:03 -0300

    Fix: Change img.src strings for better github deployment

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ $toggleSofaView.addEventListener("click", () => {
 	$sofaView.classList.toggle("360View")
 
 	if ($sofaView.classList.contains("360View")) {
-		$toggleSofaView.src = "/assets/close.svg"
+		$toggleSofaView.src = "./assets/close.svg"
 		$toggleSofaView.style.width = "1.6rem"
 		$toggleSofaView.style.heigth = "1.6rem"
 		$toggleSofaView.style.right = "5.5rem"
-		$sofaView.src = "/assets/sofaGif.gif"
+		$sofaView.src = "./assets/sofaGif.gif"
 		return
 	}
 	$toggleSofaView.style.width = "3rem"
 	$toggleSofaView.style.heigth = "2rem"
 	$toggleSofaView.style.right = "5rem"
-	$toggleSofaView.src = "/assets/360.svg"
-	$sofaView.src = "/assets/sofa.svg"
+	$toggleSofaView.src = "./assets/360.svg"
+	$sofaView.src = "./assets/sofa.svg"
 })
